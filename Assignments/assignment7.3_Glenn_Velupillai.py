d5ede5f8e15fd29de9db2eda7f36884d52e2199a
glennsvel90 <glennselwynvel@gmail.com>
2016-04-26 13:12:29 -0400

    73

--- Assignments/assignment7.3_Glenn_Velupillai.py
+++ Assignments/assignment7.3_Glenn_Velupillai.py
@@ -1,2 +1,21 @@
 file_name=raw_input("Enter file name: ")
-fh=open(file_name)
+fh=open('mbox-short.txt')
+numl=[]
+count=0
+sum=0
+for line in fh:
+
+	line= "mbox-short.txt".find(".")
+	n=float(line)
+
+	number = float("mbox-short"[line:])
+
+	put=type(number)
+	numl.append(put)
+print "Before", count, sum
+for value in numl:
+	count =count+1
+	sum= sum+value
+	print count, sum, value
+print "After", count, sum, sum / count
+
