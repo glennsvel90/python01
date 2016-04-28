0ee2c0fe9408477c1b89ec8b702a946534d2e69f
glennsvel90 <glennselwynvel@gmail.com>
2016-04-28 18:53:55 -0400

    8.5

--- Assignments/assignment7.3_Glenn_Velupillai.py
+++ Assignments/assignment7.3_Glenn_Velupillai.py
@@ -1,29 +1,19 @@
-file_name=raw_input("Enter file name: ")
-<<<<<<< HEAD
-fh=open(file_name)
-
-for line in fh:
-	decimal=line.find(.)
-	number=float(line[decimal:])
-=======
-fh=open('mbox-short.txt')
+fh=open("mbox-short.txt")
 numl=[]
-count=0
-sum=0
+
 for line in fh:
+	line = line.strip()
+	if line.startswith("X-DSPAM-Confidence:"):
+		decimal=line.find(".")
+		number=line[decimal:]
+		number=float(number)
+		numl.append(number)
 
-	line= "mbox-short.txt".find(".")
-	n=float(line)
 
-	number = float("mbox-short"[line:])
-
-	put=type(number)
-	numl.append(put)
-print "Before", count, sum
+count = 0
+sumof = 0
 for value in numl:
 	count =count+1
-	sum= sum+value
-	print count, sum, value
-print "After", count, sum, sum / count
->>>>>>> ee305d8dc5e532c6d27f4ce28c75e99c0427abbb
+	sumof= sumof +value
+print sumof / count
 
